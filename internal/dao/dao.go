730aad38afe3947d508701c4668a6c43d5f2137d
kazura233 <kazira233@gmail.com>
2020-12-27 06:02:41 +0800

    完善数据库配置

--- internal/dao/dao.go
+++ internal/dao/dao.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 	"shippo-server/configs"
 	"shippo-server/internal/model"
 	"shippo-server/utils"
@@ -28,7 +29,12 @@ func New() (d *Dao) {
 		DontSupportRenameIndex:    true,     // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,     // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false,    // 根据当前 MySQL 版本自动配置
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "shippo_",
+			SingularTable: true, // 使用单数表名
+		},
+	})
 
 	if err != nil {
 		panic(err)
