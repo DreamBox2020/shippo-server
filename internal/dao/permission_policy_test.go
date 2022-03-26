061173386b0a9f460bfc0cfc9180b53456a4fea0
kazura233 <kazira233@gmail.com>
2022-03-27 01:19:15 +0800

    查询全部权限策略，扩展状态字段（该角色是否拥有当前策略）

--- internal/dao/permission_policy_test.go
+++ internal/dao/permission_policy_test.go
@@ -18,3 +18,9 @@ func TestPermissionPolicyDao_PermissionPolicyFind(t *testing.T) {
 	res, _ := d.Group.PermissionPolicy.PermissionPolicyFind(1)
 	fmt.Printf("PermissionPolicyFind:%+v\n", res)
 }
+
+func TestPermissionPolicyDao_PermissionPolicyFindAllExtStatus(t *testing.T) {
+	d := New()
+	res, _ := d.Group.PermissionPolicy.PermissionPolicyFindAllExtStatus(1)
+	fmt.Printf("PermissionPolicyFindAllExtStatus:%+v\n", res)
+}
