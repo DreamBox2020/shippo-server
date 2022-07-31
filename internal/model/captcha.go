0ba7c5566013f56827afa3b0be4059ee34c01193
kazura233 <kazira233@gmail.com>
2022-07-31 20:40:50 +0800

    优化

--- internal/model/captcha.go
+++ internal/model/captcha.go
@@ -19,3 +19,13 @@ type Captcha struct {
 func (t *Captcha) IsExpire() bool {
 	return time.Since(t.UpdatedAt) > time.Minute*15
 }
+
+// ShadowIsExpire 影子是否失效 有效期30分钟
+func (t *Captcha) ShadowIsExpire(c *Captcha) bool {
+	// 如果影子被删除的时间，大于30分钟，则无效
+	if time.Since(c.DeletedAt.Time) > time.Minute*30 {
+		return true
+	}
+
+	return t.IsExpire()
+}
